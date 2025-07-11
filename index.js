5f9dd6e05ca623b8cd0df79629980cc7f348d178
MaishaTahsin <rubaitreshad@gmail.com>
2025-07-12 01:04:16 +0600

    Load a Single Item By ID and create an Update route

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
     // POST endpoint to receive and store user data
     app.post("/users", async (req, res) => {
       const newUser = req.body; // Get user data from request body
